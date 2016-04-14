934c8f5b06838074c3496b05746f3c176cd15fcb
Houston McClung <hmcclungiii@gmail.com>
2016-04-14 15:16:52 -0400

    Added files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace LineRepeater
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(606, 28);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "This program is used to duplicate the lines in the textbox a specified number of " +
+			"times.";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(13, 41);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(249, 23);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "In the box below, paste the text you want repeated.";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(13, 68);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(603, 187);
+			this.textBox1.TabIndex = 2;
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(496, 262);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+			this.numericUpDown1.TabIndex = 3;
+			this.numericUpDown1.Value = new decimal(new int[] {
+									3,
+									0,
+									0,
+									0});
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(214, 264);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(276, 18);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "How many times do you want each line to be repeated?";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(13, 338);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(603, 242);
+			this.textBox2.TabIndex = 5;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(13, 295);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(603, 37);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "When you click this button, your repeated lines will appear in the textbox below." +
+			"";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(496, 586);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(120, 23);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "Exit Application";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(628, 614);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.Name = "MainForm";
+			this.Text = "Line Repeater";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LineRepeater
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+			
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			textBox2.Clear();
+			foreach (string line in textBox1.Lines) {
+				for (int i = 0; i < numericUpDown1.Value; i++) {
+					string lineout = line;
+					if (textBox2.TextLength != 0) { lineout = Environment.NewLine + line; }
+					textBox2.AppendText(lineout);
+				}
+			}
+		}
+	}
+}
